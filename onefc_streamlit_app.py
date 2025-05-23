353d9b25adefe50586fd0977e25ff3f806418d85
rhodzx <80139638+rhodzx@users.noreply.github.com>
2025-05-23 17:01:56 +0800

    Update onefc_streamlit_app.py

--- onefc_streamlit_app.py
+++ onefc_streamlit_app.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 import pandas as pd
 
-st.set_page_config(page_title="ONE FC Name Translator", page_icon="🥋")
+st.set_page_config(page_title="ONE Name Translator", page_icon="🥋")
 
 st.title("🥋 ONE Athlete Name Translator")
 url = st.text_input("Paste the ONE athlete URL:", "https://www.onefc.com/athletes/rodtang/")
